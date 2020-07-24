2aab347292beab2e2d886eec2e1eb24a16e67e1c
SamuelParks <Samuel.Steele.Parks@gmail.com>
2020-07-24 15:51:09 -0400

    Commit 1
    We are interested in developing a dashboard to visualize tsunami data for events that occurred during 2000-2020. We chose tsunamis because most natural disaster dashboards focus on earthquakes. While tsunamis are usually caused by earthquakes, we thought it would be interesting to look at data that is tsunami-specific, including how far away effects were observed compared to the source location, water height, injuries, and damages.   Tsunami data is available on the National Oceanic and Atmospheric Administration (NOAA) website: https://www.ngdc.noaa.gov/hazel/view/hazards/tsunami/runup-search/  We are also web scraping the latest tsunami warning, advisory, watch, or threat headline from https://www.tsunami.gov/

---
+++ Website-Data_Testing/static/js/amit_graphs.js
@@ -0,0 +1,94 @@
+function graphTsunamisByYear(data) {
+  var yearTotals = [];
+  var counter = 0;
+  var currentYear = 0;
+
+  for(i = 0; i < data.length; i++) {
+    if (i == 0) {
+      // Initialize for first time
+      currentYear = data[i]["Year"];
+      if (data[i]["Maximum Water Height (m)"] > 0) {
+        counter += 1;
+      }
+    } // end if
+    else {  
+      if (data[i]["Year"] == currentYear) {
+        if (data[i]["Maximum Water Height (m)"] > 0) {
+          counter += 1;
+        }
+        if (i == (data.length - 1)) {
+          yearTotals.push(counter);
+        }
+      }
+      else {
+        // Different year found
+        yearTotals.push(counter);
+        counter = 0;
+        currentYear = data[i]["Year"];
+      }
+    } // end else
+  } // end for
+
+  
+  x_axis = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020];
+  y_axis = yearTotals;
+
+  var trace1 = {
+    x: x_axis,
+    y: y_axis,
+    type: 'line'
+  };
+  var data = [trace1];
+  var layout = {
+      title: 'Tsunami By Year',
+      xaxis: {
+        title: 'Years'
+      },
+      yaxis: {
+        title: 'Total Number of Tsunamis'
+      }
+      };
+    
+  Plotly.newPlot('linegraph', data, layout);
+
+} // end function graphTsunamisByYear()
+
+
+function graphCountryHistogram(data) {
+  var countryArray = [];
+
+  data.forEach(function(d) {
+    if(d["Maximum Water Height (m)"] > 0) {
+      countryArray.push(d["Country"]);
+    } // end if
+
+  }) // end forEach()
+
+  var trace = {
+    x: countryArray,
+    type: 'histogram',
+  };
+  var data = [trace];
+  Plotly.newPlot('histogram', data);
+}
+
+
+
+function graphYearHistogram(data) {
+  var yearArray = [];
+
+  filteredData.forEach(function(d) {
+    if(d["Maximum Water Height (m)"] > 0) {
+      yearArray.push(d["Year"]);
+    } // end if
+
+  }) // end forEach()
+
+  console.log(yearArray);
+  var trace = {
+    x: yearArray,
+    type: 'histogram',
+  };
+  var data = [trace];
+  Plotly.newPlot('histogram', data);
+}
